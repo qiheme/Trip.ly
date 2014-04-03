3cb13f24d43dd7770995177c3310cbfd5f54cdc6
Quincy Iheme <qiheme@gmail.com>
2014-04-03 16:20:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Triply::Application.config.secret_key_base = 'afb166d154dc366f36f4edd3847bb1dbe9f1141d562125366f903eb6ada0c9ae6661cc11cff5392c9e95245ee59f2270bba3c0b0ea67cd3d63e4989400b91dd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Triply::Application.config.session_store :cookie_store, key: '_Triply_session'
