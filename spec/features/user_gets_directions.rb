6b512064293ec3c2bef8a582814cccd9cd2f5186
Quincy Iheme <qiheme@gmail.com>
2014-04-09 00:06:12 -0400

    stlyed and commented

--- spec/features/user_gets_directions.rb
+++ spec/features/user_gets_directions.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 
 describe "getting directions", :js => true  do
+  it "displays the start address" do
+    visit root_path
+    within('#addresses') do
+      fill_in 'start', :with => '24 S. Kingman Rd., S. Orange, NJ'
+    end
+    click_button 'Submit'
+    expect(page).to have_content('24 S. Kingman Rd., S. Orange, NJ')
+  end
+
+  it "displays the end address" do
+    visit root_path
+    within('#addresses') do
+      fill_in 'finish', :with => '10 E. 21 St., New York, NY'
+    end
+    click_button 'Submit'
+    expect(page).to have_content('10 E. 21 St., New York, NY')
+  end
+
   it "displays the directions" do
     visit root_path
-    # expect('#directionsPanel').to_not have_content
-
     within('#addresses') do
       fill_in 'start', :with => '24 S. Kingman Rd., S. Orange, NJ'
       fill_in 'finish', :with => '10 E. 21 St., New York, NY'
     end
     click_button 'Submit'
-    expect(page).to have_content('25 Kingman Road South')
+    expect(page).to have_content('24 Kingman Road South')
   end
 end
